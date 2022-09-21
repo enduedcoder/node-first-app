4dd2ca8a33db03bbf3482e38c1cf0fad0711130d
Nitisha Holkar <nitishaholkar.work@gmail.com>
2022-09-21 07:49:17 +0530

    Add files

---
+++ routes/shop.js
@@ -0,0 +1,19 @@
+const path = require("path");
+const express = require("express");
+const router = express.Router();
+const rootDir = require("../utils/path");
+const adminData = require("./admin");
+
+// router.get("/", (req, res, next) => {
+//   console.log(adminData.products);
+//   res.sendFile(path.join(rootDir,"views","shop.html"));
+// });
+
+// module.exports = router;
+
+router.get("/", (req, res, next) => {
+  console.log("shop.js", adminData.products);
+  res.sendFile(path.join(rootDir, "views", "shop.html")); //sent to client browser
+});
+
+module.exports = router;
